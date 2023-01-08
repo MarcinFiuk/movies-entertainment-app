d06acbd5d145834fdaf9aec530527e3907d33fcf
Marcin_Fi <fiukmarcinn@gmail.com>
2023-01-08 11:05:39 +0000

    :refactor: change in implementation react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,34 @@
-import styled from 'styled-components';
-import { Routes, Route } from 'react-router-dom';
+import {
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+} from 'react-router-dom';
 
-import Header from './components/Header';
-import Home from './pages/Home';
 import { DataProvider } from './context/dataContext';
-import Bookmarked from './pages/Bookmarked';
-import SearchBar from './components/searchBar';
+
+import RootLayout from './layouts/RootLayout';
+import Home from './pages/Home';
 import Category from './pages/Category';
+import Bookmarked from './pages/Bookmarked';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<RootLayout />}>
+            <Route index element={<Home />} />
+            <Route path='/movies' element={<Category />} />
+            <Route path='/series' element={<Category />} />
+            <Route path='/bookmarked' element={<Bookmarked />} />
+        </Route>
+    )
+);
 
 function App() {
     return (
         <DataProvider>
-            <Wrapper>
-                <Header />
-                <main>
-                    <SearchBar />
-                    <Routes>
-                        <Route path='/' element={<Home />} />
-                        <Route path='/movies' element={<Category />} />
-                        <Route path='/series' element={<Category />} />
-                        <Route path='/bookmarked' element={<Bookmarked />} />
-                    </Routes>
-                </main>
-            </Wrapper>
+            <RouterProvider router={router} />
         </DataProvider>
     );
 }
 
 export default App;
-
-const Wrapper = styled.div`
-    display: grid;
-    grid-template-columns: auto;
-    grid-template-rows: auto 1fr;
-    gap: 1.5rem;
-
-    @media (min-width: 48rem) {
-        grid-template-rows: auto 1fr;
-        gap: 2rem;
-    }
-
-    @media (min-width: 64rem) {
-        grid-template-columns: 96px 1fr;
-        grid-template-rows: auto;
-        gap: 32px;
-    }
-`;
--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Reset } from './styles/Reset';
 import { GlobalStyle } from './styles/Global';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Reset />
-            <GlobalStyle />
-            <App />
-        </BrowserRouter>
+        <Reset />
+        <GlobalStyle />
+        <App />
     </React.StrictMode>
 );
 
